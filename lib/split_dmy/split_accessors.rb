bff925c74265e3bf06ba1ac1b13fc40cc16bff28
Colin Bruce <colinbruce@gmail.com>
2015-08-02 20:52:56 +0100

    refactoring code to reduce duplication and complexity

--- lib/split_dmy/split_accessors.rb
+++ lib/split_dmy/split_accessors.rb
@@ -4,86 +4,89 @@ module SplitDmy
       require 'date_validator'
 
       attrs.each do |attr|
-        after_initialize do
-          full_date = send("#{attr}")
-          unless full_date.nil?
-            instance_variable_set("@#{attr}_day", full_date.day)
-            instance_variable_set("@#{attr}_month", full_date.month)
-            instance_variable_set("@#{attr}_year", full_date.year)
-          end
-        end
 
-        validate "validate_#{attr}_partials".to_sym
+        extend_builtin(attr)
 
-        define_method("#{attr}=") do |val|
-          super(val)
-          unless val.nil?
-            instance_variable_set("@#{attr}_day", Date.parse(val.to_s).day)
-            instance_variable_set("@#{attr}_month", Date.parse(val.to_s).month)
-            instance_variable_set("@#{attr}_year", Date.parse(val.to_s).year)
-          end
-        end
+        add_attr_accessors(attr)
 
-        define_method("#{attr}_day=") do |val|
-          instance_variable_set("@#{attr}_day", val)
-          new = DateValidator.new(self, attr).partial_updated
-          send("#{attr}=", new)
-        end
+        extend_validation(attr)
+      end
+    end
 
-        define_method("#{attr}_month=") do |val|
-          instance_variable_set("@#{attr}_month", val)
-          new = DateValidator.new(self, attr).partial_updated
-          send("#{attr}=", new)
-        end
+    private
 
-        define_method("#{attr}_year=") do |val|
-          instance_variable_set("@#{attr}_year", val)
-          new = DateValidator.new(self, attr).partial_updated
-          send("#{attr}=", new)
-        end
+    def extend_validation(attr)
+      validate "validate_#{attr}_partials".to_sym
 
-        ['day', 'month', 'year'].each do |part|
-          define_method("#{attr}_#{part}") do
-            instance_variable_get("@#{attr}_#{part}")
-          end
-        end
-
-        define_method("validate_#{attr}_partials") do
-          dv = DateValidator.new(self, attr)
-
-          new_errs = []
-
-          if dv.all_partials_empty?
-            new_errs << "you need to provide a valid date"
-          elsif dv.partials_valid? && !dv.build_date
-            temp_date = [instance_variable_get("@#{attr}_day"),
-                         instance_variable_get("@#{attr}_month"),
-                         instance_variable_get("@#{attr}_year")].join('-')
-            new_errs << "'#{temp_date}' is not a valid date"
-          else
-            field_errors = []
-            ['day', 'month', 'year'].each do |part|
-              err_msg = ''
-              if instance_variable_get("@#{attr}_#{part}").to_s.empty?
-                err_msg = 'must be completed'
-                field_errors << "#{part} #{err_msg}"
-              else
-                unless DateValidator.new(self, attr).send("valid_#{part}?")
-                  err_msg = "is not a valid #{part}"
-                  field_errors << "'#{instance_variable_get("@#{attr}_#{part}")}' #{err_msg}"
-                end
+      define_method("validate_#{attr}_partials") do
+        dv = DateValidator.new(self, attr)
+        new_errs = []
+        if dv.all_partials_empty?
+          new_errs << 'you need to provide a valid date'
+        elsif dv.partials_valid? && !dv.build_date
+          temp_date = [send("#{attr}_day"),
+                       send("#{attr}_month"),
+                       send("#{attr}_year")].join('-')
+          new_errs << "'#{temp_date}' is not a valid date"
+        else
+          field_errors = []
+          %w(day month year).each do |part|
+            if instance_variable_get("@#{attr}_#{part}").to_s.empty?
+              err_msg = 'must be completed'
+              field_errors << "#{part} #{err_msg}"
+            else
+              unless DateValidator.new(self, attr).send("valid_#{part}?")
+                err_msg = "is not a valid #{part}"
+                field_errors << err_msg
               end
-              errors.add("#{attr}_#{part}".to_sym, err_msg) if err_msg.present?
             end
-
-            new_errs << "#{field_errors.to_sentence(last_word_connector: ' and ')}" unless field_errors.empty?
-
+            errors.add("#{attr}_#{part}".to_sym, err_msg) if err_msg.present?
           end
 
-          unless new_errs.empty?
-            errors.delete(attr.to_sym)
-            errors.add(attr.to_sym, "is not valid, #{new_errs.to_sentence(last_word_connector: ' and ')}")
+          unless field_errors.empty?
+            err_msg = field_errors.to_sentence(last_word_connector: ' and ')
+            new_errs << err_msg
           end
+        end
+
+        unless new_errs.empty?
+          errors.delete(attr.to_sym)
+          err_msg = new_errs.to_sentence(last_word_connector: ' and ')
+          errors.add(attr.to_sym, "is not valid, #{err_msg}")
+        end
+      end
+    end
+
+    def extend_builtin(attr)
+      after_initialize do
+        full_date = send("#{attr}")
+        unless full_date.nil?
+          instance_variable_set("@#{attr}_day", full_date.day)
+          instance_variable_set("@#{attr}_month", full_date.month)
+          instance_variable_set("@#{attr}_year", full_date.year)
+        end
+      end
+
+      define_method("#{attr}=") do |val|
+        super(val)
+        unless val.nil?
+          instance_variable_set("@#{attr}_day", Date.parse(val.to_s).day)
+          instance_variable_set("@#{attr}_month", Date.parse(val.to_s).month)
+          instance_variable_set("@#{attr}_year", Date.parse(val.to_s).year)
+        end
+      end
+    end
+
+    def add_attr_accessors(attr)
+      %w(day month year).each do |part|
+        define_method("#{attr}_#{part}=") do |val|
+          instance_variable_set("@#{attr}_#{part}", val)
+          new = DateValidator.new(self, attr).partial_updated
+          send("#{attr}=", new)
+        end
+
+        define_method("#{attr}_#{part}") do
+          instance_variable_get("@#{attr}_#{part}")
         end
       end
     end
